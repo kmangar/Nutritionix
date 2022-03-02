18ede883a9ec8dcec786ea5ad1f47929739444f5
Kham Mangar <kham.mangar@outlook.com>
2022-03-01 22:16:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 
 GENDER = "male"
@@ -8,7 +10,8 @@ AGE = Your Age
 APP_ID = "Your APPID"
 API_KEY = "your API KEY"
 
-exercise_endpooint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/yourendpoint/myWorkouts/workouts"
 
 excerise_text = input("Tell me which exercises you did: ")
 
@@ -20,14 +23,33 @@ parameters = {
     "age": AGE
 }
 
-headers = {
+sheet_headers = {"Authorization": "Bearer MYREALLYLONGTOKENIGOT" }
+
+exercise_headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
     "x-remote-user-id": "0"
 }
 
-response = requests.post(exercise_endpooint, json=parameters, headers=headers)
+response = requests.post(exercise_endpoint, json=parameters, headers=exercise_headers)
 
 data = response.json()
 print(data)
 
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in data["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=sheet_headers)
+
+    print(sheet_response.text)
